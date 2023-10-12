5b02e677361e4f9eaa14422fee609519bdcb6389
JorgeRodriguezPrado <jrodriguezprado@frba.utn.edu.ar>
2023-10-12 11:14:00 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int main(){
     double pi=0;
     double nro=1;
-    for (int i = 0 ; abs(n*4) > 0.0000001; i++)
+    for (int i = 0 ; abs(nro*4) > 0.0000001; i++)
     {
         nro = pow(-1,i)/(2*i+1);
-        pi+= n;
+        pi+= nro;
 
     }
     cout << setprecision(6)<< fixed << pi*4 ;
